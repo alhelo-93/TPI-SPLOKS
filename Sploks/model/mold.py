4a18c7070d0a1d859665c9a7deadf70f2f797524
alhelo-93 <47849816+alhelo-93@users.noreply.github.com>
2022-05-16 14:20:18 +0200

    create new function for update

--- Sploks/model/mold.py
+++ Sploks/model/mold.py
@@ -39,11 +39,56 @@ def selectWithParamsgroupby(columns, table, params=""):
         "Unknown error"
 
 
-def updateOne(table, field, values, params=" "):
+def updateOneField(table, field, values, params=" "):
     try:
         query = f"UPDATE {table} SET {field}='{values}' {params}"
         cur.execute(query)
         con.commit()
+        return True
+    except mysql.connector.Error as sqlError:
+        print(sqlError)
+        return False
+    except:
+        "Unknown error"
+
+
+def updateOne(table, values, params=""):
+    """
+    Update a table in the database
+
+    :param table: The name of the table to update
+    :param values: The values to be updated
+    :param params: The WHERE clause
+    """
+    try:
+        query = f"UPDATE {table} SET {values} {params}"
+        cur.execute(query)
+        con.commit()
+    except mysql.connector.Error as sqlError:
+        print(sqlError)
+        return True
+    except:
+        return True
+
+
+
+
+
+def createOne(table, columns, values):
+    """
+    Create a new row in a table
+
+    :param table: The name of the table you want to insert into
+    :param columns: The columns to insert into
+    :param values: The values to insert into the table
+    :return: The last row id of the table.
+    """
+    try:
+        query = f"INSERT INTO {table} ({columns}) VALUES ({values})"
+
+        cur.execute(query)
+        con.commit()
+        return cur.getlastrowid()
     except mysql.connector.Error as sqlError:
         print(sqlError)
     except:
